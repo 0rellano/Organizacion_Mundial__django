60f4b4b3eca708fbc541d4ff1f88f00932695223
Lucio Peralta <lucio.peralta.ar@gmail.com>
2023-08-25 10:12:56 -0300

    Creacion de modelos y migracion

--- organizacion_mundial/models.py
+++ organizacion_mundial/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Formacion(models.Model):
+    delanteros = models.IntegerField()
+    defensas = models.IntegerField()
+    mediocampistas = models.IntegerField()
+
+
+class Pais(models.Model):
+    nombre = models.CharField(max_length=50)
+    continente = models.CharField(max_length=20)
+    abreviatura = models.CharField(max_length=5)
+    formacion = models.OneToOneField(Formacion, on_delete=models.CASCADE)
+
+
+class Jugadores(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    fecha_nacimiento = models.DateField()
+    numero_camiseta = models.IntegerField()
+    pais = models.ForeignKey(Pais, on_delete=models.CASCADE)
