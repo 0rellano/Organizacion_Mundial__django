c52f903b388dc57439bfd959032552fe7afba7ea
Lucio Peralta <lucio.peralta.ar@gmail.com>
2023-10-20 11:18:27 -0300

    Correcion de footer y creacion de plantillas

--- organizacion_mundial/urls.py
+++ organizacion_mundial/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', homeView.as_view(), name='index'),
     path('jugadores/', ListaJugadoresView.as_view(), name='lista_jugadores'),
     path('jugadores/<int:pk>/', DetalleJugadorView.as_view(), name='detalle_jugador'),
-    path('paises/', ListaPaisesView.as_view()),
+    path('paises/', ListaPaisesView.as_view(), name='paises'),
     path('pais/<int:pk>', PaisView.as_view()),
-    path('mundiales/', ListaMundialesView.as_view())
+    path('mundiales/', ListaMundialesView.as_view(), name='mundiales'),
 ]
