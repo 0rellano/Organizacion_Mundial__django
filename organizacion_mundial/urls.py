f16d8e26882cae9c321c84b71e00e076e88453c5
Lucio Peralta <110489538+LucioPeralta@users.noreply.github.com>
2023-10-27 11:04:25 -0300

    Cambios de interfaz

--- organizacion_mundial/urls.py
+++ organizacion_mundial/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('jugadores/', ListaJugadoresView.as_view(), name='lista_jugadores'),
     path('jugadores/<int:pk>/', DetalleJugadorView.as_view(), name='detalle_jugador'),
     path('paises/', ListaPaisesView.as_view(), name='paises'),
-    path('pais/<int:pk>', PaisView.as_view()),
+    path('pais/<int:pk>', PaisView.as_view(), name='pais'),
     path('mundiales/', ListaMundialesView.as_view(), name='mundiales'),
     path('mundiales/<int:pk>', DetalleMundialView.as_view(), name='mundial'),
     path('registro/', Registro.as_view(), name='registro'),
